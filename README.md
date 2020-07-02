b9a25c01eb0dda83d4939b35a52225cb998b40a5
facebadge <facebadge@mobilana.mobi>
2020-07-02 19:04:55 +0300

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Pull Request WebHook
+# assay.it webhook
 
+The action triggers quality assessment job at https://assay.it
+
+## Usage
+
+See [action.yml](action.yml)
+
+Supported triggers:
+* pull_request
+
+```yaml
+on: [pull_request]
+
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: assay-it/github-actions-webhook@v1
+        with:
+          secret: ${{ secrets.ASSAY_SECRET_KEY }}
+```
--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -l
-
 set -eu
-
 export PATH=$PATH:/go/bin
 
+##
+## 
 if [ "${GITHUB_EVENT_NAME}" != "pull_request" ] ;
 then
   echo "Event ${GITHUB_EVENT_NAME} is not supported"
   exit 128
 fi
 
+##
+##
 HEAD_SRC=$(jq -r '.pull_request.head.repo.full_name' < ${GITHUB_EVENT_PATH})
 HEAD_REF=$(jq -r '.pull_request.head.ref' < ${GITHUB_EVENT_PATH})
 HEAD_SHA=$(jq -r '.pull_request.head.sha' < ${GITHUB_EVENT_PATH})
@@ -24,6 +26,8 @@ TITLE=$(jq -r '.pull_request.title' < ${GITHUB_EVENT_PATH})
 echo "==> base: ${BASE_SRC}/${BASE_REF}/${BASE_SHA}"
 echo "==> head: ${HEAD_SRC}/${HEAD_REF}/${HEAD_SHA}"
 
+##
+##
 assay \
   -api latest.assay.it \
   -secret $1 \

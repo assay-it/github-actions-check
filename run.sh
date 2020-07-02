3012b1afb42f164103a60165ddd055dc54a21048
facebadge <facebadge@mobilana.mobi>
2020-07-02 18:21:52 +0300

    fix path to jq

--- run.sh
+++ run.sh
@@ -6,16 +6,16 @@ then
   exit 128
 fi
 
-HEAD_SRC=$(jq '.pull_request.head.repo.full_name' < ${GITHUB_EVENT_PATH})
-HEAD_REF=$(jq '.pull_request.head.ref' < ${GITHUB_EVENT_PATH})
-HEAD_SHA=$(jq '.pull_request.head.sha' < ${GITHUB_EVENT_PATH})
+HEAD_SRC=$(/go/bin/jq '.pull_request.head.repo.full_name' < ${GITHUB_EVENT_PATH})
+HEAD_REF=$(/go/bin/jq '.pull_request.head.ref' < ${GITHUB_EVENT_PATH})
+HEAD_SHA=$(/go/bin/jq '.pull_request.head.sha' < ${GITHUB_EVENT_PATH})
 
-BASE_SRC=$(jq '.pull_request.head.repo.full_name' < ${GITHUB_EVENT_PATH})
-BASE_REF=$(jq '.pull_request.head.ref' < ${GITHUB_EVENT_PATH})
-BASE_SHA=$(jq '.pull_request.head.sha' < ${GITHUB_EVENT_PATH})
+BASE_SRC=$(/go/bin/jq '.pull_request.head.repo.full_name' < ${GITHUB_EVENT_PATH})
+BASE_REF=$(/go/bin/jq '.pull_request.head.ref' < ${GITHUB_EVENT_PATH})
+BASE_SHA=$(/go/bin/jq '.pull_request.head.sha' < ${GITHUB_EVENT_PATH})
 
-NUMBER=$(jq '.number' < ${GITHUB_EVENT_PATH})
-TITLE=$(jq '.pull_request.title' < ${GITHUB_EVENT_PATH})
+NUMBER=$(/go/bin/jq '.number' < ${GITHUB_EVENT_PATH})
+TITLE=$(/go/bin/jq '.pull_request.title' < ${GITHUB_EVENT_PATH})
 
 
 /go/bin/assay \

38b2b7585051b4ed43fd78c6e6e236464ded4637
facebadge <facebadge@mobilana.mobi>
2020-07-02 17:54:19 +0300

    try out an action with minimal setup

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -l
 
-/go/bin/assay \
-  -api latest.assay.it \
-  -secret $1 \
-  -head $2 \
-  -base $3 \
-  -number $4 \
-  -title $5
+env
+
+# /go/bin/assay \
+#   -api latest.assay.it \
+#   -secret $1 \
+#   -head $2 \
+#   -base $3 \
+#   -number $4 \
+#   -title $5

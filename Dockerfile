fcab963ec5df53f0a759399f89a62a462631dab5
facebadge <facebadge@mobilana.mobi>
2020-07-02 18:18:07 +0300

    parse event with jq

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM golang
 
-RUN go get github.com/assay-it/assay
+RUN set -eu \
+  && curl -o /go/bin/jq -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 \
+  && chmod +x /go/bin/jq \
+  && go get github.com/assay-it/assay
+
 COPY run.sh /run.sh
 
 ENTRYPOINT ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,13 +1,27 @@
 #!/bin/sh -l
 
-env
+if [ "${GITHUB_EVENT_NAME}" != "pull_request" ] ;
+then
+  echo "Event ${GITHUB_EVENT_NAME} is not supported"
+  exit 128
+fi
 
-cat ${GITHUB_EVENT_PATH}
+HEAD_SRC=$(jq '.pull_request.head.repo.full_name' < ${GITHUB_EVENT_PATH})
+HEAD_REF=$(jq '.pull_request.head.ref' < ${GITHUB_EVENT_PATH})
+HEAD_SHA=$(jq '.pull_request.head.sha' < ${GITHUB_EVENT_PATH})
 
-# /go/bin/assay \
-#   -api latest.assay.it \
-#   -secret $1 \
-#   -head $2 \
-#   -base $3 \
-#   -number $4 \
-#   -title $5
+BASE_SRC=$(jq '.pull_request.head.repo.full_name' < ${GITHUB_EVENT_PATH})
+BASE_REF=$(jq '.pull_request.head.ref' < ${GITHUB_EVENT_PATH})
+BASE_SHA=$(jq '.pull_request.head.sha' < ${GITHUB_EVENT_PATH})
+
+NUMBER=$(jq '.number' < ${GITHUB_EVENT_PATH})
+TITLE=$(jq '.pull_request.title' < ${GITHUB_EVENT_PATH})
+
+
+/go/bin/assay \
+  -api latest.assay.it \
+  -secret $1 \
+  -head ${HEAD_SRC}/${HEAD_REF}/${HEAD_SHA} \
+  -base ${BASE_SRC}/${BASE_REF}/${BASE_SHA} \
+  -number ${NUMBER} \
+  -title ${TITLE}

7704ec3c4baae0d5d96c0e074ee660740a528a22
facebadge <facebadge@mobilana.mobi>
2020-07-02 18:23:06 +0300

    fix path env variable

--- run.sh
+++ run.sh
@@ -1,24 +1,25 @@
 #!/bin/sh -l
 
+export PATH=$PATH:/go/bin
+
 if [ "${GITHUB_EVENT_NAME}" != "pull_request" ] ;
 then
   echo "Event ${GITHUB_EVENT_NAME} is not supported"
   exit 128
 fi
 
-HEAD_SRC=$(/go/bin/jq '.pull_request.head.repo.full_name' < ${GITHUB_EVENT_PATH})
-HEAD_REF=$(/go/bin/jq '.pull_request.head.ref' < ${GITHUB_EVENT_PATH})
-HEAD_SHA=$(/go/bin/jq '.pull_request.head.sha' < ${GITHUB_EVENT_PATH})
+HEAD_SRC=$(jq '.pull_request.head.repo.full_name' < ${GITHUB_EVENT_PATH})
+HEAD_REF=$(jq '.pull_request.head.ref' < ${GITHUB_EVENT_PATH})
+HEAD_SHA=$(jq '.pull_request.head.sha' < ${GITHUB_EVENT_PATH})
 
-BASE_SRC=$(/go/bin/jq '.pull_request.head.repo.full_name' < ${GITHUB_EVENT_PATH})
-BASE_REF=$(/go/bin/jq '.pull_request.head.ref' < ${GITHUB_EVENT_PATH})
-BASE_SHA=$(/go/bin/jq '.pull_request.head.sha' < ${GITHUB_EVENT_PATH})
+BASE_SRC=$(jq '.pull_request.head.repo.full_name' < ${GITHUB_EVENT_PATH})
+BASE_REF=$(jq '.pull_request.head.ref' < ${GITHUB_EVENT_PATH})
+BASE_SHA=$(jq '.pull_request.head.sha' < ${GITHUB_EVENT_PATH})
 
-NUMBER=$(/go/bin/jq '.number' < ${GITHUB_EVENT_PATH})
-TITLE=$(/go/bin/jq '.pull_request.title' < ${GITHUB_EVENT_PATH})
+NUMBER=$(jq '.number' < ${GITHUB_EVENT_PATH})
+TITLE=$(jq '.pull_request.title' < ${GITHUB_EVENT_PATH})
 
-
-/go/bin/assay \
+assay \
   -api latest.assay.it \
   -secret $1 \
   -head ${HEAD_SRC}/${HEAD_REF}/${HEAD_SHA} \
